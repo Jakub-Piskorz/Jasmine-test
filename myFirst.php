d64673eda288d51edf3068b6e26f441754c5eb52
Jakub Piskorz <kontakt@jakubpiskorz.pl>
2020-04-26 01:56:42 +0200

    my first PHP file

---
+++ myFirst.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Despite everyone claiming PHP is dead...</p>
+    <?php echo "<p>...PHP controls 76% of web backend market in 2020.</p>"?>
+
+</body>
+</html> 
